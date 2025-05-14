c2600769558338b8be58deaeb9584aee0fb80dc7
Laurent Arifaj <40305216+laurentarifaj1111@users.noreply.github.com>
2025-05-14 22:17:42 +0200

    rename

--- hill_climbing_with_greedy_initial/models/Results.py
+++ hill_climbing_with_greedy_initial/models/Results.py
@@ -1,6 +1,6 @@
 
 class Results:
-    def __init__(self,instance_name:str,combination: str,version1_score: int):
+    def __init__(self,instance_name:str,combination: str,fitness: int):
         self.instance_name = instance_name
         self.combination = combination
-        self.version1_score = version1_score
+        self.fitness = fitness
